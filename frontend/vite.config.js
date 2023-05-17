367fda2f0a4906bbb32a1ff2a2781ab7014b501a
Christa <christa@Zipcoders-MBP-6.fios-router.home>
2023-05-16 23:56:09 -0400

    Using certs on the filesystem instead of basic-ssl plugin.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    https: true,
+    https: {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    }
   },
-  plugins: [react(),basicSsl()],
+  plugins: [react()],
 })
